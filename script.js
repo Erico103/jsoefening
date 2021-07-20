e26ebaaaf08fe2a84b3cc2ed37de33fd0219becf
Erico103 <85289051+Erico103@users.noreply.github.com>
2021-07-20 15:43:36 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const age = 16;
+const isFemale = false;
+const driverStatus = false
+
+if(age>18){
+    console.log("Je bent ouder dan 18, dus vermaak je!!")
+}else{
+    console.log("Je bent jonger dan 18, sorry je mag niet naar binnen!")
+}
+
+if(isFemale){
+    console.log("Leuk dat je er bent, kom binnen!")
+}else{
+    console.log("Ladiesnight dude! Gaat heen!!")
+}
+
+if(driverStatus){
+    console.log("Jij bent de Bob dus jij mag rijden")
+}else{
+    console.log("Jij bent te dronken dus jij mag niet rijden")
+}
